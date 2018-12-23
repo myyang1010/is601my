51f7c9ee3841f04887b1e22013ec601638ff29fd
myyang1010 <my254@njit.edu>
2018-12-23 07:48:01 -0500

    added includes,layout and pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    welcome to the homepage
                 </div>
 
                 <div class="links">
